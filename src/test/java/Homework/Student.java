96a0e34b32b2ad80d2496ea8d143193c117c9ba5
Baros2535 <brsaltn.25@gmail.com>
2021-09-08 01:22:32 +0300

    07092021

---
+++ src/test/java/Homework/Student.java
@@ -0,0 +1,14 @@
+package Homework;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    private int number;
+    private String firstname,lastname;
+
+}
